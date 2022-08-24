3d19273b8713bad89017907450fb059987db78f5
faik-irkham <faikirkham@gmail.com>
2022-08-24 18:04:50 +0700

    connecting the cart provider

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
 enum FilterOptions {
   Favorite,
@@ -36,6 +39,18 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                   child: Text('Only Favorite'), value: FilterOptions.Favorite),
               PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
             ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {},
+            ),
           ),
         ],
       ),
