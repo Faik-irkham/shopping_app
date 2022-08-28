b2a65da061e78435fe5e715aaacaef58a1b8fb9f
faik-irkham <faikirkham@gmail.com>
2022-08-28 13:19:36 +0700

    update via patch ke server

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -124,9 +124,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  void updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url =
+          'https://shop-app-2b76a-default-rtdb.asia-southeast1.firebasedatabase.app/$id.json';
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
